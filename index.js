84ab66066da18d6a5971ba057808a375b453dd82
+ <familymanjosh@gmail.com>
2022-12-16 10:31:07 -0800

    commit tree 41b9a381c23256242db67cd2eb3ec8e7b667ae68

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber<=400) {
+    return 'This one is on me!'
+    
+  }
+  if(someNumber<=2000){
+    return 'That will be twenty bucks.'
+  }
+  if(someNumber<2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  if(someNumber>2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(NYC){
+  let city = (NYC)
+  return city=='NYC' ? 'Ok, sounds good.' : 'No go.';
+  }
 
-function switchOnCharmFromTip(){
   // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch (tip){
+  case "generous": 
+  return "Thank you so much.";
+  case "not as generous": 
+  return "Thank you.";
+  case "thanks for everything": 
+  return "Bye.";
+  default: "Thank you so much."
+   }
+  }
